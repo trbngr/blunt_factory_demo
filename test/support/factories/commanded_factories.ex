c984418e43b0066b7604297b84aaf55cba649255
Chris Martin <chris.martin@oforce.com>
2022-07-01 11:47:59 -0700

    comments

--- test/support/factories/commanded_factories.ex
+++ test/support/factories/commanded_factories.ex
@@ -2,6 +2,55 @@ defmodule Factories.CommandedFactories do
   defmacro __using__(_opts) do
     quote do
       # This factory will build the FailureContext struct
+      # It will generate the following function
+
+      # def failure_context_factory(input) do
+      #   factory = %Blunt.Data.Factories.Factory{
+      #     active_builder: nil,
+      #     data: %{},
+      #     field_validations: nil,
+      #     fields: nil,
+      #     final_message: nil,
+      #     message_name: nil,
+      #     input: input,
+      #     name: :failure_context_factory,
+      #     opts: [debug: false],
+      #     values: [
+      #       %Blunt.Data.Factories.Values.Defaults{
+      #         values:
+      #           Enum.into([application: :factory_demo, handler_state: %{}, handler_name: nil], %{})
+      #       },
+      #       %Blunt.Data.Factories.Values.Prop{
+      #         lazy: false,
+      #         field: :stacktrace,
+      #         path_func_or_value: fn _data -> %{} end
+      #       },
+      #       %Blunt.Data.Factories.Values.Prop{field: :context, path_func_or_value: %{}, lazy: true},
+      #       %Blunt.Data.Factories.Values.Prop{field: :metadata, path_func_or_value: %{}, lazy: true},
+      #       %Blunt.Data.Factories.Values.Prop{
+      #         lazy: false,
+      #         field: :test_name,
+      #         path_func_or_value: fn x1 -> build(:my_test, x1) end
+      #       },
+      #       %Blunt.Data.Factories.Values.Prop{
+      #         lazy: false,
+      #         field: :name,
+      #         path_func_or_value: [:test_name, :person_name]
+      #       }
+      #     ],
+      #     message: Commanded.Event.FailureContext,
+      #     operation: :build,
+      #     factory_module: All,
+      #     builders: [
+      #       Blunt.Data.Factories.Builder.EctoSchemaBuilder,
+      #       Blunt.Data.Factories.Builder.StructBuilder,
+      #       Blunt.Data.Factories.Builder.MapBuilder
+      #     ]
+      #   }
+
+      #   Blunt.Data.Factories.Factory.build(factory)
+      # end
+
       factory Commanded.Event.FailureContext, debug: false do
         # set defaults
         defaults application: :factory_demo,
